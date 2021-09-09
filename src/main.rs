e209d656b4cbd5c5b71a4e18a560f4827ba71965
Michael <3533648+hertg@users.noreply.github.com>
2021-09-09 11:08:11 +0200

    add pager widget

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,24 @@ use cnx::{Cnx, Position};
 
 fn main() -> Result<(), Error> {
     let attr = Attributes {
-        font: Font::new("Envy Code R 21"),
+        font: Font::new("monospace"),
         fg_color: Color::white(),
-        bg_color: None,
+        bg_color: Some(Color::black()),
+        padding: Padding::new(8.0, 8.0, 8.0, 8.0),
+    };
+
+    let pager_attr = Attributes {
+        font: Font::new("Ubuntu Mono Bold 14"),
+        fg_color: Color::white(),
+        bg_color: Some(Color::blue()),
         padding: Padding::new(8.0, 8.0, 0.0, 0.0),
     };
 
+    let mut p2_attr = pager_attr.clone();
+    p2_attr.bg_color = None;
+
     let mut cnx = Cnx::new(Position::Top);
+    cnx.add_widget(Pager::new(pager_attr, p2_attr));
     cnx.add_widget(ActiveWindowTitle::new(attr.clone()));
     cnx.add_widget(Clock::new(attr.clone(), Option::None));
     cnx.run()?;
